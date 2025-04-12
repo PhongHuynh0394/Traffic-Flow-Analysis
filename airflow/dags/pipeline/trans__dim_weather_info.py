890170e0c2cf09fdf1a89d4ba385dab19b1040dd
PhongHuynh0394 <phonghuynh9403@gmail.com>
2025-04-12 16:43:42 +0700

    added crawling weather

--- airflow/dags/pipeline/trans__dim_weather_info.py
+++ airflow/dags/pipeline/trans__dim_weather_info.py
@@ -2,7 +2,6 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-from airflow.models.param import Param
 from airflow.decorators import task
 from datetime import datetime
 import pandas as pd
@@ -10,8 +9,9 @@ import logging
 import os
 import time
 
-PSQL_TABLE = "tbl__raw__dim_cam_info"
+PSQL_TABLE = "tbl__raw__dim_weather_info"
 PSQL_CONN_ID = "conn_psql__raw_crawl"
+
 
 default_args = {
     "owner": "phonghuynh",
@@ -21,13 +21,12 @@ default_args = {
 
 
 with DAG(
-    'trans__traffic_image_id',  
+    'trans__dim_weather_info',  
     default_args=default_args,
-    description='Crawling traffic image id',
+    description='Crawl dim weather info',
     schedule_interval=None,  
     start_date=datetime(2023, 4, 5),  
     catchup=False,  
-    params={"limit": Param(default=-1, type="integer")}
 ) as dag:
 
     start_task = DummyOperator(
@@ -37,26 +36,28 @@ with DAG(
     create_table_psql = SQLExecuteQueryOperator(
         task_id="create_psql_table",
         conn_id=PSQL_CONN_ID,
-        sql=f"CREATE TABLE IF NOT EXISTS {PSQL_TABLE} (id varchar(255) PRIMARY KEY, location varchar(255), district varchar(255));"
+        sql = f"""
+            CREATE TABLE IF NOT EXISTS {PSQL_TABLE} (
+                id SERIAL PRIMARY KEY,
+                district_url TEXT,
+                district varchar(255),
+                city varchar(255),
+                url TEXT
+            )
+        """
     )
 
 
     @task(provide_context=True)
-    def cam_id_crawling(limit: int = -1):
-        from utils.crawling import TrafficCrawler
+    def weather_id_crawling():
+        from utils.crawling import WeatherCrawler
 
-        crawler = TrafficCrawler()
-        data = crawler.get_cam_info(limit=limit)
+        crawler = WeatherCrawler()
+        data = crawler.get_city_list()
 
         df = pd.DataFrame(data)
-        df.rename(columns={
-            "CamId": "id",
-            "DisplayName": "location",
-            "Disctrict": "district"
-        }, inplace=True)
-        df.fillna(value="unknown", inplace=True)
 
-        return df[['id', 'location', 'district']]
+        return df[["district_url", "district", "city", "url"]]
 
 
     @task()
@@ -68,28 +69,28 @@ with DAG(
         # TRUNCATE
         hook.run(f"TRUNCATE TABLE {PSQL_TABLE};")
         
-        temp_csv_path = '/tmp/temp_data.tsv'
-        data.to_csv(temp_csv_path, sep='\t', index=False, header=False)
-        logging.info(f"Write data to {temp_csv_path}")
+        temp_path = '/tmp/temp_weather.tsv'
+        data.to_csv(temp_path, sep='\t', index=True, header=False)
+        logging.info(f"Write data to {temp_path}")
 
         try:
             time.sleep(100)
-            hook.bulk_load(table=PSQL_TABLE, tmp_file=temp_csv_path)
+            hook.bulk_load(table=PSQL_TABLE, tmp_file=temp_path)
             logging.info(f"Successfully loaded data into {PSQL_TABLE}")
         except Exception as e:
             logging.error(f"Failed to load data into {PSQL_TABLE}: {e}")
             raise
         finally:
-            if os.path.exists(temp_csv_path):
-                logging.info(f"Delete temp file {temp_csv_path}")
-                os.remove(temp_csv_path)
+            if os.path.exists(temp_path):
+                logging.info(f"Delete temp file {temp_path}")
+                os.remove(temp_path)
 
 
     end_task = DummyOperator(
         task_id='end'
     )
 
-    cam_id = cam_id_crawling(limit="{{params.limit}}")
-    ingest_psql_task = ingest_psql(cam_id)
+    weather_id = weather_id_crawling()
+    ingest_psql_task = ingest_psql(weather_id)
 
-    start_task >> create_table_psql >> cam_id >> ingest_psql_task >> end_task
+    start_task >> create_table_psql >> weather_id >> ingest_psql_task >> end_task
