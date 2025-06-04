bcb27df31202840072f0b1f255a15046bb72903e
Huỳnh Lưu Vĩnh Phong <111982291+PhongHuynh0394@users.noreply.github.com>
2025-06-04 12:40:52 +0700

    Revert "Feature/streaming"

--- airflow/dags/utils/crawling/exception.py
+++
@@ -1,8 +0,0 @@
-class HTTP429TooManyRequests(Exception):
-    pass
-
-class HTTP404NotFound(Exception):
-    pass
-
-class HTTP403Forbidden(Exception):
-    pass
--- model_service/api/healthcheck.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter, File, UploadFile
-router = APIRouter()
-
-@router.get("/healthcheck")
-async def healthcheck():
-    return {"status": "ok", "message": "Service is running"}
